deb339591ca92973e63143b66b729eb72c1328b7
Birk Karlsen-Bæck <birk.karlsen.baeck@gmail.com>
2023-04-25 19:43:09 +0200

    [analytical_functions] made functions to convert between dB and linear.

---
+++ beam_dynamics_tools/analytical_functions/mathematical_functions.py
@@ -0,0 +1,27 @@
+'''
+Useful mathematical functions.
+
+Author: Birk Emil Karlsen-Bæck
+'''
+
+import numpy as np
+
+
+def to_dB(x):
+    r'''
+    Converts a linear signal to dB.
+
+    :param x: numpy-array signal
+    :return: numpy-array signal in dB
+    '''
+    return 20 * np.log10(x)
+
+
+def to_linear(x):
+    r'''
+    Converts a signal in dB to linear.
+
+    :param x: numpy-array signal in dB
+    :return: numpy-array signal in linear
+    '''
+    return 10**(x/20)
